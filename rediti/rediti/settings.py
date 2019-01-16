a0b65a3585a608c2605e4346831831546af1ba01
JecaTatu <gas5@cin.ufpe.br>
2019-01-16 06:31:42 -0300

    correções nos models

--- rediti/rediti/settings.py
+++ rediti/rediti/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'common',
     'users',
+    'subs',
 ]
 
 MIDDLEWARE = [
--- rediti/subs/models.py
+++ rediti/subs/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from common.models import IndexedTimeStampModel
+from common.models import IndexedTimeStampedModel
 
 
-class Subred(model.Models):
+class Subred(models.Model):
     title = models.CharField(max_length=50)
     creator = models.ForeignKey("users.User", related_name="Subreds", on_delete=models.CASCADE)
     description = models.TextField()
@@ -12,34 +12,34 @@ class Subred(model.Models):
         return self.title
     
 
-class Thread(model.Models):
+class Thread(IndexedTimeStampedModel):
     title = models.CharField(max_length=50)
     author = models.ForeignKey("users.User", related_name="threads", on_delete=models.CASCADE)
     Subred = models.ForeignKey('Subred', related_name="threads", on_delete=models.CASCADE)
     vote_count = models.IntegerField(default=0)
 
     class Meta:
-        ordering = -['created']
+        ordering = ['-created']
 
     def __str__(self):
         return self.title
     
-class Post(model.Models):
+class Post(IndexedTimeStampedModel):
     content = models.TextField()
     author = models.ForeignKey("users.User", related_name="posts", on_delete=models.CASCADE)
     thread = models.ForeignKey('Thread', related_name="posts", on_delete=models.CASCADE)
     vote_count = models.IntegerField(default=0)
 
     class Meta:
-    ordering = ['created']
+        ordering = ['created']
 
     def __str__(self):
         return ("{} - {} - {}").format(self.id, self.thread, self.author)
 
 
 class Subscription(IndexedTimeStampedModel):
-    user = models.ForeignKey('users.User', related_name=subscription, on_delete=models.CASCADE)
-    sub = models.ForeignKey('Subred', related_name=subscription, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', related_name="subscription", on_delete=models.CASCADE)
+    sub = models.ForeignKey('Subred', related_name="subscription", on_delete=models.CASCADE)
     subscribed = models.BooleanField(default=False)
 
     class Meta:
